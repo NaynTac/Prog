28e09da2d22f60037ca50b8f16e60bddeec7a787
mipka <pimenovegor65@gmail.com>
2025-05-29 22:50:59 +0300

    Create cmake_clean.cmake

---
+++ Лабораторная работа №5/DVD_Project/cmake_build/CMakeFiles/dvd_screensaver.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dvd_screensaver.dir/graphics.c.o"
+  "CMakeFiles/dvd_screensaver.dir/graphics.c.o.d"
+  "CMakeFiles/dvd_screensaver.dir/main.c.o"
+  "CMakeFiles/dvd_screensaver.dir/main.c.o.d"
+  "dvd_screensaver.exe"
+  "dvd_screensaver.pdb"
+  "libdvd_screensaver.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dvd_screensaver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
