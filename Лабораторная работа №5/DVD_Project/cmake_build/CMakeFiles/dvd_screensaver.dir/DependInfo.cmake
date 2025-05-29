1c195d9291521c4e643d2d3e5d7845f658da8bd0
mipka <pimenovegor65@gmail.com>
2025-05-29 22:51:08 +0300

    Create DependInfo.cmake

---
+++ Лабораторная работа №5/DVD_Project/cmake_build/CMakeFiles/dvd_screensaver.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/c/Users/NaNTc/Рабочий стол/Программирование 1об_ИВТ-2/Программирование (C)/DVD_Project/graphics.c" "CMakeFiles/dvd_screensaver.dir/graphics.c.o" "gcc" "CMakeFiles/dvd_screensaver.dir/graphics.c.o.d"
+  "/c/Users/NaNTc/Рабочий стол/Программирование 1об_ИВТ-2/Программирование (C)/DVD_Project/main.c" "CMakeFiles/dvd_screensaver.dir/main.c.o" "gcc" "CMakeFiles/dvd_screensaver.dir/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
